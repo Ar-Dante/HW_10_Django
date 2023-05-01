ba1332d10f88a8658aea79f1262ba9f86b439472
Artem <atlas.monker@gmail.com>
2023-05-01 17:56:47 +0300

    add db

--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
 from django.shortcuts import render
 
+from .utils import get_mongodb
 
-def main(request):
-    return render(request, 'quotes/base.html', context={})
+
+def main(request, ):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    return render(request, 'quotes/index.html', context={'quotes': quotes})
