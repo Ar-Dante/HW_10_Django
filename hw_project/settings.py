997f0c95ac19cbbbec9424150107e32de9bf29ce
Artem <atlas.monker@gmail.com>
2023-05-01 22:40:33 +0300

    add login and register

--- hw_project/settings.py
+++ hw_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quotes',
-    'users'
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/users/login"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
